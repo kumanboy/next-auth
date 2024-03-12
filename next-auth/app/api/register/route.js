4132deecf783b8a4d3b4112849a3ff03db9ab552
kumanboy <husandavronov13@gmail.com>
2024-03-12 12:09:15 +0500

    api done

---
+++ next-auth/app/api/register/route.js
@@ -0,0 +1,29 @@
+import {NextResponse} from "next/server";
+import connectDB from "@/config/db";
+import User from "@/model/User";
+import bcrypt from "bcryptjs"
+
+export const POST = async (request) => {
+    const {username, email, password, confirmPassword} = await request.json()
+
+    if (password !== confirmPassword) {
+        return new NextResponse(JSON.stringify({error: "Password do not mathc"}, {status: 400}))
+    }
+    await connectDB()
+    const exitingUser = await User.findOne({email})
+    if (exitingUser) {
+        return new NextResponse(JSON.stringify({error: "User already exists"}, {status: 409}))
+    }
+
+//     codeni hashlash
+    const hashedPassword = await bcrypt.hash(password,10)
+    const newUser = new User({username,email, password: hashedPassword})
+
+    try {
+        await newUser.save()
+        return new NextResponse('User Successfully created', {status: 201})
+    }
+    catch (error) {
+        return new NextResponse(error, {status: 500})
+    }
+}
---
+++ next-auth/app/register/page.js
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Register() {
+    return (
+        <div>
+            <h1>Register</h1>
+        </div>
+    )
+}
