a636240d2a53ef96802ba3592682a3fe93d0c0a7
kumanboy <husandavronov13@gmail.com>
2024-03-12 11:04:41 +0500

    setup

---
+++ next-auth/model/User.js
@@ -0,0 +1,9 @@
+import mongoose, {Schema} from 'mongoose'
+
+const userSchema = new Schema({
+    email: {type: String, required: true},
+    password: {type: String, required: false},
+}, {timestamps: true});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+export default User
